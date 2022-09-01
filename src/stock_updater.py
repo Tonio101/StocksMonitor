41373d8cde276de0d4419934d3aca4e30b44fcbe
Tonio101 <antonio@antonio101.com>
2022-08-31 23:50:06 -0700

    Stop logging ticker data during off hours

--- src/stock_updater.py
+++ src/stock_updater.py
@@ -1,3 +1,6 @@
+import datetime
+import pytz
+import holidays
 import yfinance as yf
 
 from time import sleep
@@ -6,6 +9,9 @@ from logger import Logger
 log = Logger.getInstance().getLogger()
 
 TIMER_SLEEP_MIN = (5 * 60)
+MARKET_CLOSE_SLEEP = (30 * 60)
+TIME_ZONE = pytz.timezone('US/Pacific')
+US_HOLIDAYS = holidays.US()
 
 
 class StockStats(Thread):
@@ -16,8 +22,32 @@ class StockStats(Thread):
         self.influxdb_clients = influxdb_clients
         self.timer = timer
 
+    def is_market_close(self, now=None):
+        if not now:
+            now = datetime.datetime.now(TIME_ZONE)
+        open_time = datetime.time(hour=6, minute=30, second=0)
+        close_time = datetime.time(hour=13, minute=0, second=0)
+
+        # If its a holiday
+        if now.strftime('%Y-%m-%d') in US_HOLIDAYS:
+            return True
+
+        # If before 06:30 or after 16:00
+        if (now.time() < open_time) or (now.time() > close_time):
+            return True
+
+        # If its the weekend
+        if now.date().weekday() > 4:
+            return True
+
+        return False
+
     def run(self):
         while True:
+            if self.is_market_close():
+                sleep(MARKET_CLOSE_SLEEP)
+                continue
+
             try:
                 if self.influxdb_clients:
                     # Inject to InfluxDB
