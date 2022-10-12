37f715aad1cee2d5ef9312ac39e1745149f3ada8
Tonio101 <antonio@antonio101.com>
2022-10-11 21:17:35 -0700

    Give container name

--- start_docker_container.sh
+++ start_docker_container.sh
@@ -5,4 +5,4 @@ IMAGE_ID=$(docker images -q $IMAGE_TAG)
 
 echo -n "Image ID: $IMAGE_ID"
 #docker run -d --network=host --restart on-failure:5 ${IMAGE_ID}
-docker run -d --network=host --restart on-failure ${IMAGE_ID}
+docker run -d --network=host --name stock_crypto_monitor --restart on-failure ${IMAGE_ID}
